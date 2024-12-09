393e298f7abf6f3cb9a1a15a3730efc7b3c3640f
Beatpc <correiabeatriz2001@gmail.com>
2024-12-09 19:01:00 +0000

    Game setup

---
+++ src/App.java
@@ -0,0 +1,13 @@
+// Main file where the code will be run
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int rowCount = 21;
+        int columnCount = 19;
+        int tileSize = 32;
+        int boardWidth = columnCount * tileSize;
+        int boardHeight = rowCount * tileSize;
+
+    }
+}
