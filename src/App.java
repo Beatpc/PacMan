46e5c65aa13080c5c9abeccbf972a86262f3496b
Beatpc <correiabeatriz2001@gmail.com>
2024-12-09 19:27:39 +0000

    Created the game window pop-up

--- src/App.java
+++ src/App.java
@@ -3,11 +3,19 @@ import javax.swing.JFrame;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        int rowCount = 21;
+        // set the dimensions of the game window
         int columnCount = 19;
+        int rowCount = 21;
         int tileSize = 32;
         int boardWidth = columnCount * tileSize;
         int boardHeight = rowCount * tileSize;
 
+        // create the game window
+        JFrame frame = new JFrame("Pac Man Game");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
